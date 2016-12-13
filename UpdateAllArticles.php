d08d9b30390096c4b8952d4854b8bde7c1ad3fc3
Ruchika Nagarkar <ruchikanagarkar@outlook.com>
2016-12-13 15:06:00 +0530

    files added

---
+++ UpdateAllArticles.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * Script for updating articles in Joomla with external php file placed in the root directory of Joomla package
+*/
+
+if (!defined('_JEXEC')) {
+    define( '_JEXEC', 1 );
+    define('JPATH_BASE', realpath(dirname(__FILE__)));
+    require_once ( JPATH_BASE .'/includes/defines.php' );
+    require_once ( JPATH_BASE .'/includes/framework.php' );
+    defined('DS') or define('DS', DIRECTORY_SEPARATOR);
+}
+
+$app = JFactory::getApplication('site');
+
+$basePath = JPATH_ADMINISTRATOR.'/components/com_content';
+require_once $basePath.'/models/article.php';
+$article_model =  JModelLegacy::getInstance('Article','ContentModel');
+$db = JFactory::getDbo();
+
+
+/*
+* Example: 1 is your starting article id and 1000 is ending article id
+*/
+for($i=1; $i<1000;$i++){
+	$query = $db->getQuery(true);
+	$query->select('*')->from('#__content')->where('id='.$i.'');
+	$update_data = $db->setQuery($query)->loadAssoc();
+
+	if(!$article_model->save($update_data)){
+	    echo $err_msg = $article_model->getError();
+
+	}else{
+		echo '<p>article updated - '.$i.'</p>';
+	}
+}
+
+?>
---
+++ UpdateMissingAssets.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Script for updating articles to fix assets table without opening and re-saving articles.
+*/
+if (!defined('_JEXEC')) {
+    define( '_JEXEC', 1 );
+    define('JPATH_BASE', realpath(dirname(__FILE__)));
+    require_once ( JPATH_BASE .'/includes/defines.php' );
+    require_once ( JPATH_BASE .'/includes/framework.php' );
+    defined('DS') or define('DS', DIRECTORY_SEPARATOR);
+}
+
+$app = JFactory::getApplication('site');
+$db = JFactory::getDbo();
+
+
+$basePath = JPATH_ADMINISTRATOR.'/components/com_content';
+require_once $basePath.'/models/article.php';
+$article_model =  JModelLegacy::getInstance('Article','ContentModel');
+
+require_once JPATH_ADMINISTRATOR.'/components/com_aclmanager/models/diagnostic.php';
+$acl_model =  JModelLegacy::getInstance('Diagnostic','AclmanagerModel');
+$missingAssets = $acl_model->getMissingAssets();
+
+/*
+ * @missingAssets : returns array of issues present in asset table
+ *
+*/
+foreach ($missingAssets as $key => $value) {
+	//to check and update only articles. The array might contain type of category and module as well
+	if($value->type == 'article'){
+		$query = $db->getQuery(true);
+		$query->select('*')->from('#__content')->where('id='.$value->id.'');
+		$update_data = $db->setQuery($query)->loadAssoc();
+		$article_model->save($update_data);
+		echo '<p>article updated - '.$value->id.'</p>';
+	}
+}
+
+
+?>
